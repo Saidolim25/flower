72723e2d5f97a1c39a5481f8876537167e5ee71f
Saidolim25 <saidasadov2003@gmail.com>
2023-02-07 11:21:49 +0500

    finished

---
+++ app.js
@@ -0,0 +1,38 @@
+ let korzinka = [
+    
+]
+
+
+let additem = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+let additem2 = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+let additem3 = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+let additem4 = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+let additem5 = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+let additem6 = korzinka.push({
+    name: 'Шок. конфеты "Лучшей маме',
+    price:1200,
+})
+
+for(let i =0; i < korzinka.length;i++){
+    console.log(korzinka[i]);
+}
+// 
+
+// add.addEventListener('click',function(){
+// console.log(korzinka.push);
+// })const add = document.getElementById('add')
